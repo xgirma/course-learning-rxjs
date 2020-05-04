b1258911cbefc222df1650b13cf24c51f9cfc21f
Girma Nigusse <xgirma@gmail.com>
2020-05-04 18:39:59 -0400

    ch-02-02 behaviour subject

---
+++ ch-02/03/rxjs/src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { BehaviorSubject } from "rxjs";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements  OnInit{
+  title = 'rxjs';
+  observable;
+  subject;
+
+  ngOnInit() {
+    this.subject = new BehaviorSubject(0);
+    this.subject.subscribe(x => console.log("First subscribed: ", x));
+    this.subject.next(1);
+    this.subject.next(2);
+    this.subject.next(3);
+
+    this.subject.subscribe(x => console.log("Second subscribed: ", x));
+    this.subject.next(4);
+  }
+
+  ngOnDestroy() {
+    this.subject.unsubscribe();
+  }
+}
